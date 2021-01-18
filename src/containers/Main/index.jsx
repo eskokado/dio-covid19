eadc9a983e20153347db81b4561fb2ae8e29a90a
Edson Shideki Kokado <eskokado@gmail.com>
2021-01-18 12:51:27 -0300

    PWA Covid 19

---
+++ src/containers/Main/index.jsx
@@ -0,0 +1,44 @@
+import React, { memo, useState, useEffect, useCallback } from 'react'
+import Api from '../../api'
+import Board from './components/Board'
+import Panel from './components/Panel'
+import { ContainerStyled } from './style'
+
+function Main() {
+
+  const [data, setData] = useState({})
+  const [country, setCountry] = useState('brazil')
+  const updateAt = new Date().toLocaleString()
+
+
+  const getCovidData = useCallback((country) => {
+      Api.getCountry(country)
+        .then(data => setData(data))
+  }, [])
+  
+  useEffect(() => {
+    getCovidData(country)
+  }, [getCovidData, country])
+
+  const handleChange = ({ target }) => {
+    const country = target.value
+    setCountry(country)
+  }
+
+  return (
+    <ContainerStyled>
+      <div className="mb-2">
+        <Panel 
+          data={data}
+          updateAt={updateAt}
+          onChange={handleChange}
+          country={country}
+          getCoviddata={getCovidData}
+        />
+      </div>
+      <Board data={data}></Board>
+    </ContainerStyled>
+  )
+}
+
+export default memo(Main)
